483394781691d10cc118771070f1bf16fa0ad380
dileep menon <dileepmenon92@yahoo.com>
2017-03-02 01:15:42 +0530

    Modify code for large testcases

--- Python/Algorithms/Implementation/Beautiful_Triplets/solution.py
+++ Python/Algorithms/Implementation/Beautiful_Triplets/solution.py
@@ -5,11 +5,15 @@ def func(s,arr):
     arr = list(map(int,arr.split(' ')))
     triplets = []
     tup = ()
-    for i in range(n):
+    del_elements = []
+    while len(arr) != 0:
+        i = 0
         tup = (arr[i],)
+        del_elements.append(arr[i])
         t = arr[i]+d
-        while t in arr and arr.index(t)<n:
+        while t in arr and arr.index(t)<len(arr):
             tup+=(t,)
+            del_elements.append(t)
             i = arr.index(t)
             if len(tup) == 3:
                 triplets.append(tup)
@@ -17,6 +21,10 @@ def func(s,arr):
                 i = arr.index(t)
                 tup= (t,)
             t+=d
+        else:
+            for j in set(del_elements):
+                arr.remove(j)
+            del_elements = []
     print (len(set(triplets)))
 
 
