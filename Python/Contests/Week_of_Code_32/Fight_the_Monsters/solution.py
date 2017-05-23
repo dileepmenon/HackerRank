4af7780a650e67f433413601f21b85c3700ff3bb
dileep menon <dileepmenon92@yahoo.com>
2017-05-23 22:52:13 +0530

    Refactor code to deal with large test cases

--- Python/Contests/Week_of_Code_32/Fight_the_Monsters/solution.py
+++ Python/Contests/Week_of_Code_32/Fight_the_Monsters/solution.py
@@ -1,6 +1,8 @@
 #!/bin/python3
 
 import sys
+import math
+
 
 def getMaxMonsters(n, hit, t, h):
     i = 0
@@ -8,18 +10,22 @@ def getMaxMonsters(n, hit, t, h):
     coun = 0
     s = sorted(h)
     while t_i < t and i < n:
-        while s[i] > 0 and t_i < t:
-            s[i] -= hit
-            t_i += 1
+        a = int(math.ceil(s[i]/hit))
+        b = t - t_i
+        if t_i+a > t:
+            s[i] -= hit*b
+            t_i += b
+        else:
+            s[i] -= hit*a
+            t_i += a
         if s[i] <= 0:
             coun += 1
         i += 1
     return coun
+
 
 n, hit, t = input().strip().split(' ')
 n, hit, t = [int(n), int(hit), int(t)]
 h = list(map(int, input().strip().split(' ')))
 result = getMaxMonsters(n, hit, t, h)
 print(result)
-
-
