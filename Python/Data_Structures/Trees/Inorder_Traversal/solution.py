9b1a5f83f1d2da583f7df003baae1861b2be469f
dileep menon <dileepmenon92@yahoo.com>
2017-06-16 14:19:42 +0530

    Add solution file

---
+++ Python/Data_Structures/Trees/Inorder_Traversal/solution.py
@@ -0,0 +1,15 @@
+#!bin/python2
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+
+
+def inOrder(root):
+        if root.left:
+            inOrder(root.left)
+        print root.data,
+        if root.right:
+            inOrder(root.right)
