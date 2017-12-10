9f53fd52e5270126acafcc7c643923f0f623a244
dileep menon <dileepmenon92@yahoo.com>
2017-12-10 20:14:04 +0530

    Add Solution File

--- Python/Data_Structures/Queues/Queue_using_Two_Stacks/solution.py
+++ Python/Data_Structures/Queues/Queue_using_Two_Stacks/solution.py
@@ -1,14 +1,41 @@
 #!bin/python3
+
+
+class Queue:
+    def __init__(self):
+        self.st1 = []
+        self.st2 = []
+
+    def enqueue(self, x):
+        if not self.st1:
+            self.st1.append(x)
+            self.front = x
+        else:
+            self.st1.append(x)
+
+    def transfer_from_st1_to_st2(self):
+        while self.st1:
+            e = self.st1.pop()
+            self.st2.append(e)
+
+    def dequeue(self):
+        if not self.st2:
+            self.transfer_from_st1_to_st2()
+        return self.st2.pop()
+
+    def peek(self):
+        if self.st2:
+            return self.st2[-1]
+        return self.front
+
+
 num_queries = int(input())
-st = []
+q = Queue()
 for i in range(num_queries):
     query = list(map(int, (input().split(' '))))
-    inst = query[0]
-    if len(query) > 1:
-        num = query[1]
-        st.append(num)
+    if query[0] == 1:
+        q.enqueue(query[1])
+    elif query[0] == 2:
+        q.dequeue()
     else:
-        if inst == 2:
-            st = st[1:]
-        else:
-            print(st[0])
+        print(q.peek())
